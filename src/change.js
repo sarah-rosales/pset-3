815daa324275f88470ef994fbeb97dbbc7776d6a
Sarah Rosales <srosales@ucvts.org>
2019-10-28 13:22:58 -0400

    exercise 1 wip

--- src/change.js
+++ src/change.js
@@ -3,14 +3,22 @@ const readlineSync = require("readline-sync");
 const MIN = 0.01;
 const MAX = 0.99;
 
+const quarters = 0.25
+const dimes = 0.10
+const nickels = 0.05
+const pennies = 0.01
+
 const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 
 if (Number.isNaN(amount)) {
-    // print your error message here
-} else if (amount < MIN || amount > MAX) {
-    // print your error message here
-} else {
-    // write your code for making change here
-}
+    console.log("\nInvalid.")
 
-console.log("\n quarters, dimes, nickels, pennies.")
+} else if (amount < MIN || amount > MAX) {
+    console.log("\nInvalid.")
+
+} else { (amount % quarters > MIN || amount/quarters < MAX)
+        qvalue = amount % quarters
+
+    console.log("\n" + qvalue + " quarters, dimes, nickels, pennies.")
+
+}
