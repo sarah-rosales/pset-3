83b6be0deb8c7ec33d20d1c5031cafd7e459edfd
Sarah Rosales <srosales@ucvts.org>
2019-10-30 12:40:57 -0400

    exercise 1

--- src/change.js
+++ src/change.js
@@ -8,17 +8,37 @@ const dimes = 0.10
 const nickels = 0.05
 const pennies = 0.01
 
-const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
+let qvalue = 0
+let dvalue = 0
+let nvalue = 0
+let pvalue = 0
+
+let amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 
 if (Number.isNaN(amount)) {
-    console.log("\nInvalid.")
-
+  console.log("\nInvalid.")
 } else if (amount < MIN || amount > MAX) {
-    console.log("\nInvalid.")
+  console.log("\nInvalid.")
+} else {
+  while (amount > quarters) {
+    amount = amount - quarters;
+    qvalue++;
+  }
 
-} else { (amount % quarters > MIN || amount/quarters < MAX)
-        qvalue = amount % quarters
+  while (amount > dimes) {
+    amount = amount - dimes;
+    dvalue++;
+  }
 
-    console.log("\n" + qvalue + " quarters, dimes, nickels, pennies.")
+  while (amount > nickels) {
+    amount = amount - nickels;
+    nvalue++;
+  }
 
+  while (amount > 0) {
+    amount = amount - pennies;
+    pvalue++;
+  }
+
+  console.log("\n" + qvalue + " quarters, " + dvalue + " dimes, "+ nvalue +" nickels, and " + pvalue + " pennies.")
 }
