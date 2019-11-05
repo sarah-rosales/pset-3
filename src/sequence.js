4d1f50fe8f938c28fe63bc4a4598877ce27397aa
Sarah Rosales <srosales@ucvts.org>
2019-11-04 20:06:26 -0500

    exercise 4

--- src/sequence.js
+++ src/sequence.js
@@ -7,8 +7,36 @@ const third_number = Number(readlineSync.question(""));
 const MIN = Number.MIN_SAFE_INTEGER
 const MAX = Number.MAX_SAFE_INTEGER
 
-if (first_number < second_number || second_number < third_number) {
+if (Number.isNaN(first_number)) {
+  console.log("\nInvalid.")
+} else if (first_number < MIN || first_number > MAX) {
+  console.log("\nInvalid.")
+}
+if (Number.isNaN(second_number)) {
+  console.log("\nInvalid.")
+} else if (second_number < MIN || second_number > MAX) {
+  console.log("\nInvalid.")
+}
+if (Number.isNaN(third_number)) {
+  console.log("\nInvalid.")
+} else if (third_number < MIN || third_number > MAX) {
+  console.log("\nInvalid.")
+}
+
+if (first_number < second_number && second_number < third_number) {
   console.log("\nStrictly increasing.");
-// } else {
-//   console.log("\nInvalid.");
-// }
+} else if (first_number == second_number && second_number < third_number) {
+  console.log("\nIncreasing.");
+} else if (first_number < second_number && second_number == third_number) {
+  console.log("\nIncreasing.");
+} else if (first_number == second_number && second_number == third_number) {
+  console.log("\nEqual.")
+} else if (first_number == second_number && second_number > third_number) {
+  console.log("\nDecreasing.");
+} else if (first_number > second_number && second_number == third_number) {
+  console.log("\nDecreasing.");
+} else if (first_number > second_number && second_number > third_number) {
+  console.log("\nStrictly decreasing.");
+} else {
+  console.log("\nUnordered.");
+}
