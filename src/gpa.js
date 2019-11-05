3907fae3baadc51b12f303b8f09b049987bac5e9
Sarah Rosales <srosales@ucvts.org>
2019-11-04 20:56:07 -0500

    exercise 5

---
+++ src/gpa.js
@@ -0,0 +1,33 @@
+const readlineSync = require("readline-sync");
+
+const grade = readlineSync.question("\nEnter a letter grade: ");
+
+if (grade == "A+") {
+  console.log("\nYour GPA is 4.00.");
+} else if (grade == "A") {
+  console.log("\nYour GPA is 4.00.");
+} else if (grade == "A-") {
+  console.log("\nYour GPA is 3.67.");
+} else if (grade == "B+") {
+  console.log("\nYour GPA is 3.33.");
+} else if (grade == "B") {
+  console.log("\nYour GPA is 3.00.");
+} else if (grade == "B-") {
+  console.log("\nYour GPA is 2.67.");
+} else if (grade == "C+") {
+  console.log("\nYour GPA is 2.33.");
+} else if (grade == "C") {
+  console.log("\nYour GPA is 2.00.");
+} else if (grade == "C-") {
+  console.log("\nYour GPA is 1.67.");
+} else if (grade == "D+") {
+  console.log("\nYour GPA is 1.33.");
+} else if (grade == "D") {
+  console.log("\nYour GPA is 1.00.");
+} else if (grade == "D-") {
+  console.log("\nYour GPA is 0.67.");
+} else if (grade == "F") {
+  console.log("\nYour GPA is 0.00.");
+} else {
+  console.log("\nInvalid.");
+}
